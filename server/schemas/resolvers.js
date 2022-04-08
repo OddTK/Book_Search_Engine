7adda890ea82c9dc87c8c751d9fc7c88604eaf48
Jesus <jesusbalandra@outlook.com>
2022-04-08 08:03:35 -0600

    created typeDefs and started resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,36 @@
+const { AuthorizationError } = require('apollo-server-express');
+const { Book, User } = require('../models');
+const utils = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (_root, {id}) => {
+            return await User.findById(id);
+        },
+    },
+    Mutation: {
+        login: async (_root, {email, password}) => {
+            const userFound = await User.findOne({email});
+            if (!userFound) {
+                throw new AuthorizationError('No user found with this email address!');
+            }
+            if (userFound.password === password) {
+                const token = utils.signToken(userFound.username, userFound.email, userFound._id);
+                return (token, userFound);
+            }
+            throw new AuthorizationError('You must provide correct credentials');
+        },
+        addUser: async (_root, {username, email, password}) => {
+            const user = await User.create({
+                username,
+                email,
+                password
+            })
+            const token = utils.signToken(user.username, user.email, user._id);
+            return (token, user);
+        },
+        saveBook: async (_root, {})
+    },
+}
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -26,7 +26,8 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(): User
+        saveBook(input: bookInput!): User
+        removeBook(bookId: String!): User
     }
 `;
 
