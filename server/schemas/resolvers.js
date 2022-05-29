f605dba5fca8d95fe5a38b86ee17c36af3539917
Jesus <jesusbalandra@outlook.com>
2022-05-29 14:21:19 -0600

    npm start

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -17,11 +17,7 @@ const resolvers = {
         },
       },
     Mutation: {
-<<<<<<< HEAD
-        login: async (_root, {email, password}) => {
-=======
         login: async (parent, {email, password}) => {
->>>>>>> 77f4a4e053d7e20edf8cbe9cd8c36b17fd743eae
             const userFound = await User.findOne({email});
             if (!userFound) {
                 throw new AuthenticationError('No user found with this email address!');
@@ -32,20 +28,12 @@ const resolvers = {
             }
             throw new AuthenticationError('You must provide correct credentials');
         },
-<<<<<<< HEAD
-        addUser: async (_root, args) => {
-=======
         addUser: async (parent, args) => {
->>>>>>> 77f4a4e053d7e20edf8cbe9cd8c36b17fd743eae
             const user = await User.create(args)
             const token = signToken(user);
             return (token, user);
         },
-<<<<<<< HEAD
-        saveBook: async (_root, { input }, context) => {
-=======
         saveBook: async (parent, { input }, context) => {
->>>>>>> 77f4a4e053d7e20edf8cbe9cd8c36b17fd743eae
             if (context.user) {
               const updatedUser = await User.findByIdAndUpdate(
                 { _id: context.user._id },
@@ -56,11 +44,7 @@ const resolvers = {
             }
             throw new AuthenticationError("You must be logged in");
           },
-<<<<<<< HEAD
-          removeBook: async (_root, args, context) => {
-=======
           removeBook: async (parent, args, context) => {
->>>>>>> 77f4a4e053d7e20edf8cbe9cd8c36b17fd743eae
             if (context.user) {
               const updatedUser = await User.findByIdAndUpdate(
                 { _id: context.user._id },
